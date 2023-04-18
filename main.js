fb201a5b74014b5389fd19ca500ede76d9c96f62
Luis Oliverez <louoliverez901@gmail.com>
2023-04-18 07:46:12 -0700

    added all

--- main.js
+++ main.js
@@ -18,48 +18,13 @@
         html += '</div>';
         return html;
     }
-<<<<<<< HEAD
-    return html;
-}
-
-
-let searchInput = document.querySelector('#search-input');
-searchInput.addEventListener('input', searchCoffees);
-
-function searchCoffees(){
-    let searchValue = searchInput.value.toLowerCase();
-    let filteredCoffees = coffees.filter(function(coffee){
-        return coffee.name.toLowerCase().includes(searchValue);
-    });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
-}
-
-
-
-function updateCoffees(e) {
-    e.preventDefault(); // don't submit the form, we just want to update the data
-    var selectedRoast = roastSelection.value;
-    var filteredCoffees = [];
-    if(selectedRoast === "all"){
-        filteredCoffees = coffees
-    }else{
-    coffees.forEach(function(coffee) {
-        if (coffee.roast === selectedRoast) {
-            filteredCoffees.push(coffee);
-        }
-    });
-    tbody.innerHTML = renderCoffees(filteredCoffees);
-}
-// ADDED ALL COFFEES TO 'ALL' OPTION
-=======
-        function renderCoffees(coffees) {
+    function renderCoffees(coffees) {
         var html = '';
         for (var i = coffees.length - 1; i >= 0; i--) {
             html += renderCoffee(coffees[i]);
         }
         return html;
     }
->>>>>>> 6b5764e2bfbcc838655f44454769c0441d9b63a3
 
 
     let searchInput = document.querySelector('#search-input');
@@ -78,11 +43,15 @@ function updateCoffees(e) {
         e.preventDefault(); // don't submit the form, we just want to update the data
         var selectedRoast = roastSelection.value;
         var filteredCoffees = [];
-        coffees.forEach(function (coffee) {
-            if (coffee.roast === selectedRoast) {
-                filteredCoffees.push(coffee);
-            }
-        });
+        if(selectedRoast === "all"){
+            filteredCoffees = coffees
+        }else {
+            coffees.forEach(function (coffee) {
+                if (coffee.roast == selectedRoast) {
+                    filteredCoffees.push(coffee);
+                }
+            });
+        }
         tbody.innerHTML = renderCoffees(filteredCoffees);
     }
 
